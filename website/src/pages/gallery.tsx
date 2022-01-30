78cd63ca6d9f893a5a65c1ba6e625b097cfe82d0
remorses <beats.by.morse@gmail.com>
2022-01-30 15:00:25 +0100

    website/s:

---
+++ website/src/pages/gallery.tsx
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import { useCallback, useLayoutEffect, useMemo } from 'react'
+
+function Card({}) {
+    return <div className='flex flex-col bg-gray-300 w-[200px] h-[100px]'></div>
+}
+
+function Gallery({ cards }) {
+    return (
+        <div className='flex w-full h-full flex-wrap gap-4'>
+            {cards.map((c, i) => {
+                return <Card key={i} />
+            })}
+        </div>
+    )
+}
+
+const cards = [null, null, null, null, null]
+export default function Home() {
+    const iframeSelector = '#xxx'
+    const handleResize = useCallback(
+        throttle(function handleResize() {
+            if (!parent?.document) {
+                return
+            }
+            const iframe: HTMLElement | null =
+                parent.document.querySelector(iframeSelector)
+            if (!iframe) {
+                return
+            }
+            iframe.style.height = document.body.scrollHeight + 'px'
+        }, 300),
+        [],
+    )
+    useLayoutEffect(() => {
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+    return <Gallery cards={cards} />
+}
+
+function throttle(func, timeFrame) {
+    var lastTime = 0
+    return function () {
+        let now = new Date().getTime()
+        if (now - lastTime >= timeFrame) {
+            func()
+            lastTime = now
+        }
+    }
+}
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,20 +1,66 @@
 import Head from 'next/head'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { CSSProperties } from 'react'
 
-function Card({}) {
-    return <div className='flex flex-col bg-gray-300 w-[200px] h-[100px]'></div>
-}
+export default function Home() {
+    const aspectRatio = 0.1
+    const parent: CSSProperties = {
+        position: 'relative',
+        width: '100%',
+        maxWidth: '100%',
+        height: 'auto',
+        margin: '24px auto',
+    }
+    const child: CSSProperties = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: '100%',
+        height: '100%',
+    }
+    const spacerStyle = {
+        paddingTop: Number(aspectRatio * 100).toFixed(2) + '%',
+    }
+    const iframeNode = useRef<HTMLIFrameElement>(null)
+    const [frameId] = useState(() => {
+        return 'xxx'
+    })
 
-function Gallery({ cards }) {
+    // useEffect(function _updateIframeContent() {
+    //     const iframe = iframeNode.current!
+
+    //     let doc: Document = (iframe as any).document
+    //     if (iframe.contentDocument) doc = iframe.contentDocument
+    //     else if (iframe.contentWindow) doc = iframe.contentWindow.document
+
+    //     // const gistLink = this._defineUrl()
+    //     const gistScript = `` //  `<script type="text/javascript" src="${gistLink}"></script>`
+    //     const styles = `<style></style>`
+    //     const resizeScript = `onload=`
+    //     const iframeHtml = `<html><head><base target="_parent">${styles}</head><body style="margin:0" ${resizeScript}>${gistScript}</body></html>`
+
+    //     // doc.open()
+    //     doc.onload =
+    //         ""
+    //     // doc.close()
+    // })
+
     return (
-        <div className='flex w-full h-full flex-wrap gap-4'>
-            {cards.map((c, i) => {
-                return <Card key={i} />
-            })}
+        <div className=''>
+            <iframe
+                id={frameId}
+                ref={iframeNode}
+                src={'/gallery'}
+                frameBorder={0}
+                scrolling='no'
+                // style={child}
+                className='h-auto w-full max-w-full'
+                // sandbox='allow-scripts allow-popups allow-top-navigation-by-user-activation allow-forms allow-same-origin'
+                allowFullScreen
+                // loading='lazy'
+            />
         </div>
     )
-}
-
-const cards = [null, null, null, null, null]
-export default function Home() {
-    return <Gallery cards={cards} />
 }
